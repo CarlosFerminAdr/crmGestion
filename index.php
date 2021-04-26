caa661ebe9fe314c55af97dea5a23ddfbf8548b5
CarlosFerminAdr <adri_car12@hotmail.com>
2021-04-25 19:42:28 -0500

    Version 0.1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="icon" type="image/png" href="public/img/appLogo.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "dependencias.php"; ?>
+    <title>appCRM</title>
+</head>
+<body>
+    
+</body>
+</html>
